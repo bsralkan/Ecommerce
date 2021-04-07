58177cd7e25f08caee4a2b4d2b76f5f67b433ac1
bsralkan <bsr.alkan@gmail.com>
2021-04-07 17:24:18 +0300

    passing parameters from controller to view

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AnasayfaController extends Controller
 {
     public function index(){
-        return view("anasayfa");
+        return view('anasayfa', ['isim'=>'Busra']);
     }
 }
