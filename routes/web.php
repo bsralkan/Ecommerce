f856768bcdcc2cb3b20b0cd01880d3bb9fdb6ac9
bsralkan <bsr.alkan@gmail.com>
2021-04-10 12:14:07 +0300

    Route gruplama

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,8 @@ Route::get('/sepet','SepetController@index')->name('sepet');
 Route::get('/odeme','OdemeController@index')->name('odeme');
 Route::get('/siparisler','SiparisController@index')->name('siparisler');
 Route::get('/siparisler/{id}','SiparisController@detay')->name('siparis');
-Route::get('/kullanici/oturumac','KullaniciController@giris_form')->name('kullanici.oturumac');
-Route::get('/kullanici/kaydol','KullaniciController@kaydol_form')->name('kullanici.kaydol');
+
+Route::group(['prefix'=>'kullanici'], function (){
+    Route::get('/oturumac','KullaniciController@giris_form')->name('kullanici.oturumac');
+    Route::get('/kaydol','KullaniciController@kaydol_form')->name('kullanici.kaydol');
+});
